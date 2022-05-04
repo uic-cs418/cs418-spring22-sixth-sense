846c8e3218353d43867aae650bd6933627984a26
nickdchiu <56891141+nickdchiu@users.noreply.github.com>
2022-05-04 16:57:46 -0500

    Final Report + Functions

--- functions.py
+++ functions.py
@@ -7,6 +7,8 @@ from matplotlib.pyplot import figure
 import json
 import nltk
 import sklearn
+import warnings
+
 
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
@@ -161,6 +163,7 @@ def price_extraction(df):
     df['price_raise/drop'] = np.where(df['price_raise/drop']>0,1,-1)
     return df
     plt.show()
+
 """
 This method prepares sentiment data for ML
 Input Param: df = price
@@ -172,6 +175,7 @@ def comp_score(df):
     df['comp_score'] = np.where(df['comp_score']>0,1,-1)
     return df
 #baseline ML model 
+
 def fit(X, y, z):
     index = z.max()
     length = X.size
@@ -179,17 +183,21 @@ def fit(X, y, z):
     for i in range(length):
         y_predict[i] = index
     return y_predict
+
 #converting data in a format compatible with svm
 def data_svmReliable_conversion(z):
     z= z.values
     return z.reshape(-1, 1)
+
 # learning the classifier
 def learn_classifier(X_train, y_train, kernel):
     classifier = sklearn.svm.SVC(kernel = kernel)
     return classifier.fit(X_train,y_train)
+
 #evaluate the classifier
 def evaluate_classifier(classifier, X_validation, y_validation):
     return sklearn.metrics.accuracy_score(y_validation , classifier.predict(X_validation))
+
 #finding the best kernel for svm
 def best_model_selection(kf, X, y):
     scores = []
@@ -202,7 +210,106 @@ def best_model_selection(kf, X, y):
         dict[kernel] = np.mean(scores)
         scores = []
     return max(dict , key = dict.get)
+
 #prediction on test data
 def classify(classifier, x_test):
     return classifier.predict(x_test)
-        
+
+def bar_chart(reddit, news, bitcoinTalk):
+    warnings.filterwarnings('ignore')
+    news_per_year = news.groupby(['Year']).size().to_frame('size').reset_index()
+    reddit_per_year = reddit.groupby(['Year']).size().to_frame('size').reset_index()
+    bitcoinTalk_per_year = bitcoinTalk.groupby(['Year']).size().to_frame('size').reset_index()
+    news_per_year.loc[-1] = [2015,0]
+    news_per_year.index = news_per_year.index + 1  # shifting index
+    news_per_year.sort_index(inplace=True) 
+    fig,ax = plt.subplots()
+    index = np.arange(max(len(reddit_per_year['Year']), len(news_per_year['Year']), len(bitcoinTalk_per_year['Year'])))
+    bar_width = 0.2
+    opacity = 0.8
+
+    ax.bar(index-bar_width, news_per_year['size'], width=bar_width, label='News')
+    ax.bar(index, reddit_per_year['size'], width=bar_width, label='Reddit')
+    ax.bar(index+bar_width, bitcoinTalk_per_year['size'], width=bar_width, label='Bitcoin Talk')
+    ax.set_yscale('log')
+    ax.tick_params(axis=u'both', which=u'both',length=0)
+    labels = ['','2015','','2016','','2017','','2018','']
+    #ax.set_yticklabels['','2015','','2016','','2017','','2018','']
+    ax.set_xticklabels(labels)
+
+    plt.xlabel('Year')
+    plt.ylabel('Amount of content')
+    plt.title('Amount of content regarding cryptocurrency from 2015-2018')
+    plt.legend(loc='best')
+
+    plt.show()
+
+def machine_learning(crypto_prices_df, news):
+    #price_sentiment = pd.merge(crypto_prices_df,news_reddit_bitcoin,on='Date_extracted')
+    price_sentiment = pd.merge(crypto_prices_df,news,on='Date_extracted')
+    x_train, x_test, y_train, y_test= train_test_split(price_sentiment['comp_score'], price_sentiment['price_raise/drop'])
+    y_predict = fit(x_train,y_train,crypto_prices_df['price_raise/drop'])
+    print("Baseline ML Model Accuracy: ")
+    print(sklearn.metrics.accuracy_score(y_train,y_predict))
+    #calling classifier
+    classifier = learn_classifier(data_svmReliable_conversion(x_train), data_svmReliable_conversion(y_train), 'linear') 
+    #calculating accuracy of learned classifier with training data
+    accuracy = evaluate_classifier(classifier, data_svmReliable_conversion(x_train), data_svmReliable_conversion(y_train))
+    print("Training accuracy: ", accuracy)
+    #calculating accuracy of learned classifier with testing data
+    test_accuracy = evaluate_classifier(classifier, data_svmReliable_conversion(x_test), data_svmReliable_conversion(y_test))
+    print("Test accuracy: ", test_accuracy)
+    #predicting 
+    y_pred = classify(classifier, data_svmReliable_conversion(x_test))
+    print("Prediction on test data")
+    print(y_pred)
+
+def KNN(crypto_prices_df, news):
+    price_sentiment = pd.merge(crypto_prices_df,news,on='Date_extracted')
+    x_train, x_test, y_train, y_test= train_test_split(price_sentiment['comp_score'], price_sentiment['price_raise/drop'])
+    #prediction using KNN
+    knn = KNeighborsClassifier()
+    #Train the model using the training sets
+    classifier = knn.fit(data_svmReliable_conversion(x_train), data_svmReliable_conversion(y_train))
+    #Predict the response for test dataset
+    accuracy = evaluate_classifier(classifier, data_svmReliable_conversion(x_test), data_svmReliable_conversion(y_test))
+    print('KNN Test Accuracy: ' , accuracy)
+
+def GNB(crypto_prices_df, news):
+    price_sentiment = pd.merge(crypto_prices_df,news,on='Date_extracted')
+    x_train, x_test, y_train, y_test= train_test_split(price_sentiment['comp_score'], price_sentiment['price_raise/drop'])
+    # Prediction using Gaussian Naive Bayes
+    gnb = GaussianNB()
+    #Train the model using the training sets
+    classifier = gnb.fit(data_svmReliable_conversion(x_train), data_svmReliable_conversion(y_train))
+    #Predict the response for test dataset
+    accuracy = evaluate_classifier(classifier, data_svmReliable_conversion(x_test), data_svmReliable_conversion(y_test))
+    print('Gaussian Naive Bayes Test Accuracy:' , accuracy)
+
+def data_return():
+    #Source: https://edloginova.github.io/cryptodata/
+    news = pd.read_json('cryptodata/news.json')
+    reddit = pd.read_json('cryptodata/reddit.json')
+    bitcoinTalk = pd.read_json('cryptodata/bitcointalk.json')
+    news = news[(news['date'].dt.year >= 2015) & (news['date'].dt.year <= 2018)]
+    reddit = reddit[(reddit['date'].dt.year >= 2015) & (reddit['date'].dt.year <= 2018)]
+    bitcoinTalk = bitcoinTalk[(bitcoinTalk['date'].dt.year >= 2015) & (bitcoinTalk['date'].dt.year <= 2018)]
+    news = addYearAndMonthColumns(news)
+    reddit = addYearAndMonthColumns(reddit)
+    bitcoinTalk = addYearAndMonthColumns(bitcoinTalk)
+    return news, reddit, bitcoinTalk
+
+def price_of_coin_2017_scatter(crypto_prices_df):
+    popular_cryptos = ['bitcoin','ethereum', 'tether', 'binance-coin', 'cardano']
+    prices_of_popular_cryptos = crypto_prices_df[crypto_prices_df['currency'].isin(popular_cryptos)]
+    #plots of price per year
+    price_2016 = price_of_popular_cryptos_during_a_particular_year(prices_of_popular_cryptos,2016)
+    price_2017 = price_of_popular_cryptos_during_a_particular_year(prices_of_popular_cryptos,2017)
+    price_2018 = price_of_popular_cryptos_during_a_particular_year(prices_of_popular_cryptos,2018)
+
+def sentiment_vs_price(news, crypto_prices_df):
+    popular_cryptos2 = ['bitcoin','ethereum', 'tether']
+    news_of_popular_cryptos2 = news[news['crypto'].isin(popular_cryptos2)]
+    bit = sentiment_price_of_popular_cryptos_during_a_particular_year(2017,'bitcoin',news, crypto_prices_df)
+    eth = sentiment_price_of_popular_cryptos_during_a_particular_year(2017,'ethereum',news, crypto_prices_df)
+    teth = sentiment_price_of_popular_cryptos_during_a_particular_year(2017,'tether',news, crypto_prices_df)
